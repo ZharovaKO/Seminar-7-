06e20b9a422173480f5bdec25fb0f43496b2914f
ZharovaKO <xkseo05@yandex.ru>
2022-07-18 19:17:19 +0300

    task021

---
+++ task021/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Ведите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите количество столбцов массива: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows, colums];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        avarage = (avarage + matrix[i, j]);
+    }
+    avarage = avarage / rows;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
